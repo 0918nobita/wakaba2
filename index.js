4b617e761fef1a6754121e0403a6285207bc7376
0918nobita <nobita.0918@gmail.com>
2021-04-02 16:36:27 +0900

    :tada: Initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+require('dotenv').config();
+
+const puppeteer = require('puppeteer-core');
+
+const launchBrowser = () =>
+  puppeteer.launch({
+    headless: false,
+    executablePath: process.env.CHROME_EXE_PATH,
+    ignoreDefaultArgs: ['--mute-audio'],
+  });
+
+const setupPage = async (browser) => {
+  const pages = await browser.pages();
+  const page = (pages.length > 0) ? pages[0] : await browser.newPage();
+  await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100');
+  return page;
+};
+
+const main = async () => {
+  const browser = await launchBrowser();
+  const page = await setupPage(browser);
+
+  await page.goto('https://v.ouj.ac.jp/view/ouj');
+
+  const button = await page.waitForSelector('.login-button');
+  await button.click();
+  await page.waitForNavigation({ waitUntil: 'networkidle0' });
+
+  await page.type('input[name="username"]', process.env.OUJ_USERNAME);
+  await page.type('input[name="password"]', process.env.OUJ_PASSWORD);
+  const submitButton = await page.waitForSelector('.btn-submit');
+  await submitButton.click();
+  await page.waitForNavigation({ waitUntil: 'networkidle0' });
+  console.log('Successfully logged in')
+
+  await page.goto('https://v.ouj.ac.jp/view/ouj/#/navi/player?co=8178&ct=V&ca=930');
+
+  const playButton = await page.waitForSelector('.vjs-big-play-button');
+  playButton.click();
+  console.log('Now playing...');
+};
+
+void main();
