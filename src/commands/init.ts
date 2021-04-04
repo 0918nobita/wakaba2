30462293c3fa975432fa142ee62a4882075d7c8f
0918nobita <nobita.0918@gmail.com>
2021-04-04 16:46:20 +0900

    :building_construction: vod コマンドの定義を分離した

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -1,47 +1,17 @@
-import * as E from 'fp-ts/Either';
-import * as TE from 'fp-ts/TaskEither';
-import { pipe } from 'fp-ts/function';
 import fs from 'fs';
 import { prompt } from 'inquirer';
-import path from 'path';
 
 const makeQuestion =
-    <T extends string>(type: 'input' | 'password', name: T, message?: string) =>
-        pipe(
-            TE.tryCatch(
-                (): Promise<{ [_ in T]: string }> =>
-                    prompt(message ? { type, name, message } : { type, name }),
-                String
-            ),
-            TE.map(v => v[name])
-        );
+    <T extends string>(type: 'input' | 'password', name: T, message?: string): Promise<{ [_ in T]: string }> =>
+        prompt({ type, name, message });
 
-const questions =
-    pipe(
-        makeQuestion('input', 'executablePath', 'Path to the executable of Chrome browser'),
-        TE.chain((executablePath) =>
-            pipe(
-                makeQuestion('input', 'username'),
-                TE.chain((username) =>
-                    pipe(
-                        makeQuestion('password', 'password'),
-                        TE.map((password) => ({ executablePath, username, password }))
-                    )
-                )
-            )
-        )
-    );
+export const init = async (configFilePath: string) => {
+    const { executablePath } = await makeQuestion('input', 'executablePath', '');
+    const { username } = await makeQuestion('input', 'username');
+    const { password } = await makeQuestion('password', 'password');
 
-export const init = async (configFilePath: string): Promise<void> => {
-    const result = await questions();
-
-    pipe(
-        result,
-        E.map(({ executablePath, username, password }) => {
-            fs.writeFileSync(
-                configFilePath,
-                JSON.stringify({ executablePath, ouj: { username, password } })
-            );
-        })
+    fs.writeFileSync(
+        configFilePath,
+        JSON.stringify({ executablePath, ouj: { username, password } })
     );
 };
