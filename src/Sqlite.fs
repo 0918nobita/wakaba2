843b081c7c5c0662dc093e6fa945ed4e6b8f2e95
0918nobita <nobita.0918@gmail.com>
2021-05-12 16:36:46 +0900

    :construction: プリペアドステートメントが使えるようにした

--- src/Sqlite.fs
+++ src/Sqlite.fs
@@ -2,12 +2,42 @@ module Sqlite
 
 open Fable.Core
 
+[<AbstractClass>]
+type IStatement =
+    abstract finalize: unit -> unit
+
+    [<Emit("$0.run($1...)")>]
+    abstract run: string[] -> unit
+
+type Statement(stmt: IStatement) =
+    member _.Finalize = stmt.finalize
+    member _.Run = stmt.run
+
+    interface System.IDisposable with
+        member this.Dispose() = this.Finalize()
+
+[<AbstractClass>]
 type IDatabase =
+    abstract close: unit -> unit
+    abstract prepare: string -> IStatement
     abstract run: string -> unit
+    abstract serialize: (unit -> unit) -> unit
+
+type Database(db: IDatabase) =
+    member _.Close = db.close
+    member _.Prepare(sql: string) = new Statement(db.prepare(sql))
+    member _.Run = db.run
+    member _.Serialize = db.serialize
+
+    interface System.IDisposable with
+        member this.Dispose() = this.Close()
 
 type ISqliteInstance =
     [<Emit("new $0.Database($1)")>]
     abstract Database: string -> IDatabase
+
+type SqliteInstance(sqlite: ISqliteInstance) =
+    member _.Database(name: string) = new Database(sqlite.Database(name))
 
 type ISqliteModule =
     abstract verbose: unit -> ISqliteInstance
@@ -15,4 +45,4 @@ type ISqliteModule =
 [<ImportDefault("sqlite3")>]
 let private sqlite3: ISqliteModule = jsNative
 
-let verbose = sqlite3.verbose
+let verbose () = new SqliteInstance(sqlite3.verbose())
