986782bc518f03e39539c2a0c7b89d65a9f2c9c1
0918nobita <nobita.0918@gmail.com>
2021-05-15 14:56:02 +0900

    :white_check_mark: QueryBuilder.combine 関数の単体テストを追加した

--- packages/QueryBuilder.Tests/ReflectionUtil.fs
+++ packages/QueryBuilder.Tests/ReflectionUtil.fs
@@ -3,6 +3,14 @@ module ReflectionUtil
 open System
 open System.Reflection
 
-let getPrivateStaticMethod (name : string) (moduleType : Type) : MethodInfo option =
-    let method = moduleType.GetMethod(name, BindingFlags.Static ||| BindingFlags.NonPublic)
-    if method <> null then Some method else None
+let getPrivateStaticMethod
+        (name : string)
+        (moduleType : Type) : MethodInfo option =
+    let method =
+        moduleType
+            .GetMethod(
+                name,
+                BindingFlags.Static ||| BindingFlags.NonPublic)
+    if method <> null
+    then Some method
+    else None
--- packages/QueryBuilder.Tests/UnitTest1.fs
+++ packages/QueryBuilder.Tests/UnitTest1.fs
@@ -2,6 +2,7 @@ module QueryBuilder.Tests
 
 open NUnit.Framework
 open Fable.SQLite3
+open FsCheck
 
 module QB = QueryBuilder
 
@@ -10,10 +11,30 @@ let Test1 () =
     let combineMethodInfo =
         typeof<QueryBuilder.Marker>.DeclaringType
         |> ReflectionUtil.getPrivateStaticMethod "combine"
-        |> Option.defaultWith (fun () -> failwith "Failed to get `combine` method info")
-    Assert.AreEqual(
-        "A B C",
-        combineMethodInfo.Invoke(null, [| ["A"; "B"; ""; "C"] |]))
+        |> Option.defaultWith
+            (fun () -> failwith "Failed to get `combine` method info")
+
+    let alphabeticString: Gen<string> =
+        Gen.oneof [Gen.choose (65, 90); Gen.choose (97, 122)]
+        |> Gen.map System.Convert.ToChar
+        |> Gen.arrayOf
+        |> Gen.map System.String
+
+    let ``List of alphabetic or empty string``: Arbitrary<string list> =
+        Gen.oneof
+            [
+                Gen.fresh (fun () -> Gen.eval 10 (Random.newSeed()) alphabeticString)
+                Gen.constant ""
+            ]
+        |> Gen.listOf
+        |> Arb.fromGen
+
+    Prop.forAll
+        ``List of alphabetic or empty string``
+        (fun strs ->
+            strs |> List.filter ((<>) "") |> String.concat " "
+                = (combineMethodInfo.Invoke(null, [| strs |]) :?> string))
+    |> Check.VerboseThrowOnFailure
 
 [<Test>]
 let Test2 () =
@@ -21,3 +42,8 @@ let Test2 () =
         "SELECT * FROM users",
         QB.select (QB.Table "users") (QB.ColsPattern "*")
         |> QB.build)
+
+[<Test>]
+let Test3 () =
+    let revRevIsOrig xs = xs |> List.rev |> List.rev = xs
+    Check.VerboseThrowOnFailure revRevIsOrig
