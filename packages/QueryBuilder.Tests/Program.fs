f79d6e95bdafe308faca27f54a6730b5e0940d1f
0918nobita <nobita.0918@gmail.com>
2021-05-17 19:07:52 +0900

    :sparkles: Lens の実装を実行時に導出するサンプルを追加した

--- packages/QueryBuilder.Tests/Program.fs
+++ packages/QueryBuilder.Tests/Program.fs
@@ -1,40 +1,7 @@
 module Program
 
 open System.Diagnostics
-open System.Text
-
-let private combine1 (strs : string list) : string =
-    let folder (sb : StringBuilder) (s : string) =
-        if sb.Length = 0
-        then
-            sb.Append s
-        else
-            if s = ""
-            then sb
-            else (sb.Append " ").Append s
-    strs
-    |> List.fold folder (StringBuilder())
-    |> string
-
-let private combine2 (strs : string list) : string =
-    strs
-    |> List.filter ((<>) "")
-    |> String.concat " "
-
-[<Measure>] type ms
-[<Measure>] type ns
-
-let private convertMsToNs (ms : float<ms>) : float<ns> =
-    ms * 1000000.0<ns/ms>
-
-let private measureExecutionTime (numOfTimes : int) (op : unit -> 'a) : float<ns> =
-    let stopWatch = Stopwatch.StartNew()
-    for _ in 1..numOfTimes do
-        ignore <| op ()
-    stopWatch.Stop()
-    let elapsed = 1.0<ms> * float stopWatch.ElapsedMilliseconds
-    elapsed / float numOfTimes
-    |> convertMsToNs
+open FSharp.Reflection
 
 type Getter<'s, 'a> = 's -> 'a
 type Setter<'s, 'a> = 's -> 'a -> 's
@@ -67,13 +34,60 @@ let _name = {
 
 type Message = Message of User * string
 
-let _user = {
-    Get = fun (Message (user, _)) -> user
-    Set = fun (Message (_, msg)) newUser -> Message (newUser, msg)
-}
+type DerivedLenses(typeInfo : System.Type) =
+    let unionCaseInfo = Array.head <| FSharpType.GetUnionCases typeInfo
+    let fields = unionCaseInfo.GetFields ()
+    let fieldNames = fields |> Array.map (fun prop -> prop.Name)
+
+    member _.TryGetLens name =
+        if Array.contains name fieldNames
+        then
+            let index = fields |> Array.findIndex (fun field -> field.Name = name)
+            let fieldType = (fields |> Array.find (fun field -> field.Name = name)).PropertyType
+            let getterType = FSharpType.MakeFunctionType (typeInfo, fieldType)
+            let getterValue = FSharpValue.MakeFunction (getterType, fun obj ->
+                let unionFields = snd <| FSharpValue.GetUnionFields (obj, typeInfo)
+                unionFields.[index])
+            let setterReturnType = FSharpType.MakeFunctionType (fieldType, typeInfo);
+            let setterType = FSharpType.MakeFunctionType (typeInfo, setterReturnType)
+            let setterValue = FSharpValue.MakeFunction (setterType, fun obj1 ->
+                FSharpValue.MakeFunction (
+                    setterReturnType,
+                    (fun obj2 ->
+                        let unionFields = snd <| FSharpValue.GetUnionFields (obj1, typeInfo)
+                        unionFields.[index] <- obj2
+                        FSharpValue.MakeUnion (unionCaseInfo, unionFields))))
+            let lensType = (typedefof<Lens<_,_>>.MakeGenericType [| typeInfo; fieldType |])
+            let lensValue = FSharpValue.MakeRecord (lensType, [| getterValue; setterValue |])
+            Some lensValue
+        else None
+
+let inline (?) (lenses : DerivedLenses) (prop : string) = lenses.TryGetLens prop
+
+[<Measure>] type ms
+[<Measure>] type ns
+
+let private convertMsToNs (ms : float<ms>) : float<ns> =
+    ms * 1000000.0<ns/ms>
+
+let private measureExecutionTime (numOfTimes : int) (op : unit -> 'a) : float<ns> =
+    let stopWatch = Stopwatch.StartNew()
+    for _ in 1..numOfTimes do
+        ignore <| op ()
+    stopWatch.Stop()
+    let elapsed = 1.0<ms> * float stopWatch.ElapsedMilliseconds
+    elapsed / float numOfTimes
+    |> convertMsToNs
 
 [<EntryPoint>]
 let main _ =
+    let userLenses = DerivedLenses(typeof<User>)
+    let _idDerived : Lens<User, int> = downcast Option.get userLenses?id
+    let _nameDerived : Lens<User, string> = downcast Option.get userLenses?name
+
+    let msgLenses = DerivedLenses(typeof<Message>)
+    let _user : Lens<Message, User> = downcast Option.get msgLenses?Item1
+
     let user1 = User (12, "Alice")
     let msg1 = Message (user1, "Hello, world!")
 
@@ -84,13 +98,10 @@ let main _ =
     printfn "%A" <| (_user ^|-> _name).Get msg1 // => "Alice"
     printfn "%A" <| (_user ^|-> _name).Set msg1 "Bob" // => Message (User (12, "Bob"), "Hello, world!")
 
-    let list = [""; "A"; ""; "B"; "C"; ""]
-    let numOfTimes = 100000
+    printfn "\n[Performance]"
+    measureExecutionTime 100 (fun () -> _id.Get user1) |> printfn "_id.Get: %.0fns"
+    measureExecutionTime 100 (fun () -> _idDerived.Get user1) |> printfn "_idDerived.Get: %.0fns\n"
 
-    measureExecutionTime numOfTimes (fun () -> combine1 list)
-    |> printfn "combine1: %-3.0fns"
-
-    measureExecutionTime numOfTimes (fun () -> combine2 list)
-    |> printfn "combine2: %-3.0fns"
-
+    measureExecutionTime 100 (fun () -> _name.Set user1 "Tom") |> printfn "_name.Get: %.0fns"
+    measureExecutionTime 100 (fun () -> _nameDerived.Set user1 "Tom") |> printfn "_nameDerived.Get: %.0fns"
     0
